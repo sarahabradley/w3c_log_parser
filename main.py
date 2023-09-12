04caaa832dafb0d294d6e48744b5a04e50aad4fd
sarahabradley <113841080+sarahabradley@users.noreply.github.com>
2023-09-12 23:48:40 +0100

    Return ranked list of URI stems in descending order of hits

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ import logParser
 def main():
     w3cParser = logParser.W3CParser()
     contents = w3cParser.loadDataSource('/Users/sarahbradley/Documents/Coding/test_log_files', 'w3c_example1.log')
-    print(w3cParser.countInField('cs-uri-stem'))
+    uriHitCounts = w3cParser.countInField('cs-uri-stem')
+    # Sort contents of uriHitCounts dictionary in descending order by value
+    sortedHitCounts = sorted(uriHitCounts.items(), key=lambda x:x[1], reverse=True)
+    print("\n---------------------------------------------------------------"
+          + "\nRanked list of URI stems in descending order of hits"
+          + "\n---------------------------------------------------------------\n")
+    for uri in sortedHitCounts:
+        print(f"{uri[0]}: {uri[1]}")
 
 if __name__=="__main__":
     main()
